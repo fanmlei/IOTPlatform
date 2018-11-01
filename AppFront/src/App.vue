5e17afaacdb706b855df012fea3df00415ab6e9f
FanMLei <15638515832@163.com>
2018-11-01 22:13:36 +0800

    增加前端初始代码‘

---
+++ AppFront/src/App.vue
@@ -0,0 +1,163 @@
+<!--<template>-->
+  <!--<div id="app">-->
+    <!--<img src="./assets/logo.png">-->
+
+    <!--<el-button type="primary">sou</el-button>-->
+  <!--</div>-->
+<!--</template>-->
+
+<!--<script>-->
+<!--export default {-->
+  <!--name: 'App'-->
+<!--}-->
+<!--</script>-->
+
+<!--<style>-->
+<!--#app {-->
+  <!--font-family: 'Avenir', Helvetica, Arial, sans-serif;-->
+  <!-- -webkit-font-smoothing: antialiased;-->
+  <!-- -moz-osx-font-smoothing: grayscale;-->
+  <!--text-align: center;-->
+  <!--color: #2c3e50;-->
+  <!--margin-top: 60px;-->
+<!--}-->
+<!--</style>-->
+
+
+<template>
+  <el-container>
+    <el-header>
+      <el-menu
+        :default-active="activeIndex"
+        class="el-menu-demo"
+        mode="horizontal"
+        @select="handleSelect"
+        background-color="#545c64"
+        text-color="#fff"
+        active-text-color="#ffd04b">
+        <div class="fl title">管理后台</div>
+        <div class="fr">
+          <el-menu-item index="1">处理中心</el-menu-item>
+          <el-submenu index="2">
+            <template slot="title">我的工作台</template>
+            <el-menu-item index="2-1">选项1</el-menu-item>
+            <el-menu-item index="2-2">选项2</el-menu-item>
+            <el-menu-item index="2-3">选项3</el-menu-item>
+          </el-submenu>
+          <el-menu-item index="3"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>
+        </div>
+      </el-menu>
+    </el-header>
+    <el-main>
+      <el-container>
+        <el-aside width="240px">
+          <el-row class="tac">
+            <el-menu
+              default-active="2"
+              class="el-menu-vertical-demo"
+              @open="handleOpen"
+              @close="handleClose">
+              <el-submenu index="1">
+                <template slot="title">
+                  <i class="el-icon-location"></i>
+                  <span>导航一</span>
+                </template>
+                <el-menu-item-group>
+                  <template slot="title">分组一</template>
+                  <el-menu-item index="1-1">选项1</el-menu-item>
+                  <el-menu-item index="1-2">选项2</el-menu-item>
+                </el-menu-item-group>
+                <el-menu-item-group title="分组2">
+                  <el-menu-item index="1-3">选项3</el-menu-item>
+                </el-menu-item-group>
+                <el-submenu index="1-4">
+                  <template slot="title">选项4</template>
+                  <el-menu-item index="1-4-1">选项1</el-menu-item>
+                </el-submenu>
+              </el-submenu>
+              <el-menu-item index="2">
+                <i class="el-icon-menu"></i>
+                <span slot="title">导航二</span>
+              </el-menu-item>
+              <el-menu-item index="3">
+                <i class="el-icon-setting"></i>
+                <span slot="title">导航三</span>
+              </el-menu-item>
+            </el-menu>
+          </el-row>
+        </el-aside>
+        <el-container>
+          <el-main>
+            <el-table :data="tableData">
+              <el-table-column prop="date" label="日期" width="140">
+              </el-table-column>
+              <el-table-column prop="name" label="姓名" width="120">
+              </el-table-column>
+              <el-table-column prop="address" label="地址">
+              </el-table-column>
+            </el-table>
+          </el-main>
+          <el-footer>
+            <el-pagination
+              @size-change="handleSizeChange"
+              @current-change="handleCurrentChange"
+              :current-page.sync="currentPage"
+              :page-size="100"
+              layout="total, prev, pager, next"
+              :total="1000">
+            </el-pagination>
+          </el-footer>
+        </el-container>
+      </el-container>
+    </el-main>
+  </el-container>
+</template>
+
+<script>
+  export default {
+    data() {
+      const item = {
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      };
+      return {
+        activeIndex: '2-1',
+        tableData: Array(5).fill(item),
+        currentPage: 5,
+      };
+    },
+    methods: {
+      handleSelect(key, keyPath) {
+        console.log(key, keyPath);
+      },
+      handleOpen(key, keyPath) {
+        console.log(key, keyPath);
+      },
+      handleClose(key, keyPath) {
+        console.log(key, keyPath);
+      },
+      handleSizeChange(val) {
+        console.log(`每页 ${val} 条`);
+      },
+      handleCurrentChange(val) {
+        console.log(`当前页: ${val}`);
+      }
+    }
+  }
+</script>
+
+<style>
+  .fl {
+    float: left;
+  }
+  .fr {
+    float: right;
+  }
+  .title {
+    width: 125px;
+    text-align: center;
+    line-height: 60px;
+    color: #fff;
+  }
+</style>
